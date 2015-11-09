ae2e2037fba941b95abe0b9e28db771a85b79b25
Ana Rey <anarey@gmail.com>
2015-11-09 16:34:20 +0100

    Delete the theme if it is found.

--- src/checkjson.py
+++ src/checkjson.py
@@ -5,56 +5,53 @@ def proccess_msg(blacklist, expected_values, message):
     result = 0
     cleaned_message =  "".join(message.split(" "))
 
-    ## No puede haber elememtos de blacklist.
     for elem in blacklist:
         if elem in cleaned_message:
             return -1
 
     for key in expected_values.keys():
         value = expected_values[key]
-        ##import ipdb; ipdb.set_trace();
         msg_search = "\"%s\":\"%s\"" % (key, value)
-        print "Campo que buscamos: %s" % (msg_search)
-        print "Texto: %s" % (cleaned_message)
         if (cleaned_message.find(msg_search) == -1):
-            print ("No lo encuentro")
             return -1    
 
     return result
 
+
 def main():
-	print "Recorro la plantilla"
+    print "Check all themes"
 
-	theme_file = open('themes/theme-example.json')
-	json_dic = json.load(theme_file)
-	message_json = "{\"bytes\": \"10\", \"pkts\": \"5\", \"type\": \"netflowv9\", \"l4_proto\": \"17\", \"tcp_flags\": \"0\", \"output_snmp\":\"2222\"}"
+    json_dic = {}
+    theme_file = open('themes/theme-example.json')
+    json_dic = json.load(theme_file)
+##    message_json = "{\"bytes\": \"10\", \"pkts\": \"5\", \"type\": \"netflowv9\", \"l4_proto\": \"17\", \"tcp_flags\": \"0\", \"output_snmp\":\"2222\"}"
+    message_json = "{\"bytes\": \"10\", \"pkts\": \"5\", \"type\": \"netflowv9\", \"l4_proto\": \"17\", \"tcp_flags\": \"0\",}"
+    key_delete = ""
+    result = 0
 
-	for msj_theme in json_dic:
-	    msg = json_dic[msj_theme][0]
-	    if len(msg.keys()) == 2 and "blacklist" in msg.keys() and "expected_values" in msg.keys():
-	        blacklist = msg[u'blacklist']
-	        expected_values = msg[u'expected_values']
-	        result = proccess_msg(blacklist, expected_values, message_json)
+    for msg_theme in json_dic:
+        msg = json_dic[msg_theme][0]
+        if len(msg.keys()) == 2 and "blacklist" in msg.keys() and "expected_values" in msg.keys():
+            blacklist = msg[u'blacklist']
+            expected_values = msg[u'expected_values']
+            result = proccess_msg(blacklist, expected_values, message_json) 
 
-	        if result == 0:
-	            print "message found"
-	            break
-	        else:
-	            print "sigo buscando con la siguiente plantilla"
+            if result == 0:
+                key_delete = msg_theme
+                print "message found"
+                break
 
-	        for elem in blacklist:
-	        	print elem
-	##        for elem in blacklist:
-	 ##       	print elem
-
-	        print  expected_values
-
-	if result != 0:
-		print "We do not found the message"
-	print "fin"
+    if (result == 0 and len(key_delete) > 0):
+        print "We delete the message of the batch of themes:  %s" % (json_dic[key_delete])
+        del(json_dic[key_delete])
 
 
-	theme_file.close()
+    if result == -1:
+        print "We do not found the message"
+
+    print "Themes are not deleted: \n %s" % (json_dic)
+
+    theme_file.close()
 
 if __name__ == '__main__':
     main()
