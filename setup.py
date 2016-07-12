ee47a3d935c4271a49c6cd7d8cf00dc15af0f147
Ana Rey <anarey@redborder.com>
2016-07-12 11:31:03 +0000

    Add a setup.py to install the program in the system
    python setup.py install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="pycheckjson",
+      version="0.1",
+      description="Check json file if it has an specific content",
+      author="Ana Rey",
+      author_email="anarey@redborder.com",
+      url="https://gitlab.redborder.lan/core-developers/pycheckjson",
+      license="AGPL",
+      scripts=["src/checkjson.py"],
+)
